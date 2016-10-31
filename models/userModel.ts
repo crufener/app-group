63ea22ae98d4975b8b4b7cf40adfe1a7cbbf6c4f
Craig Rufener <crufener@outlook.com>
2016-10-31 12:07:23 -0700

    required packages crypto and jsonwebtoken. Defined three methods to set a users password, authenticate, and grand an access token

--- models/userModel.ts
+++ models/userModel.ts
@@ -1,26 +1,51 @@
 import * as mongoose from 'mongoose';
+import * as crypto from 'crypto';
+import * as jwt from 'jsonwebtoken';
 
 export interface IUser extends mongoose.Document {
     username: string,
+    email: string,
     password: string,
     admin: boolean
 }
 
-let userSchema = new mongoose.Schema({
+let UserSchema = new mongoose.Schema({
     username: {
         type: String,
         required: [true, 'Must have a username'],
-        minlength: [3, 'Your username must be 3 or more characters long']
+        minlength: [3, 'Your username must be 3 or more characters long'],
+        lowercase: true
     },
-    password: {
+    email: {
         type: String,
-        required: true
+        unique: true,
+        lowercase: true
     },
+    passwordHash: String,
+    salt: String,
     admin: {
         type: Boolean,
         default: false,
         required: true
     }
 });
+/** Set the password */
+UserSchema.method('setPassword', function(password) {
+  this.salt = crypto.randomBytes(16).toString('hex');
+  this.passwordHash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+});
+/** authenticate credentials/password */
+UserSchema.method('validatePassword', function(password) {
+  let hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+  return (hash === this.passwordHash);
+});
+/** grant an access token */
+UserSchema.method('generateJWT', function() {
+  return jwt.sign({
+    id: this._id,
+    username: this.username,
+    email: this.email
+  }, 'SecretKey');
+});
 
-export default mongoose.model<IUser>('User', userSchema);
+export default mongoose.model<IUser>('User', UserSchema);
